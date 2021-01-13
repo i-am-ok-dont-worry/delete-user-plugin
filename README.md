76b2ff50834521efc3d409643f6b11e4fc6a0b4e
Mateusz <mateusz.pytel@grupakmk.pl>
2021-01-13 21:54:04 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,24 +1,19 @@
-# Plugin template
-This repository is a template for VueStorefrontApi plugin.
+# Customer delete plugin
+Plugin allows to delete a customer account from Magento.
 
 ## Entry point
 Entry point for plugin is a /src/index.js file. It contains a template function
 for api plugin.
 
-## Write a plugin
-Plugin receives various props including:
-* config - api configuration
-* db - elasticsearch client
-* router - express router
-* cache - cache manager instance
-* apiStatus - rest api response helper func
-* apiError - rest api error response helper func
-* getRestApiClient - method which returns Magento Rest Client
+## Usage
+* request account delete 
+```shell script
+curl -X POST "http://localhost:8080/api/vendor/customer-delete/{{customerId}}?token={{token}}&storeCode={{storeCode}}"
+``` 
 
-# IMPORTANT!
-- `package.json` must contain `pluginname` entry which describes plugin name
-- `package.json` must contain valid company info e.g.:
-```
-"companyname": "grupakmk"
-```
+* confirm delete
+```shell script
+curl -X POST "http://localhost:8080/api/vendor/customer-delete/{{customerId}}?token={{token}}&storeCode={{storeCode}}"
+``` 
 
+`token` is a delete token fetched from email message
--- src/index.js
+++ src/index.js
@@ -1,10 +1,69 @@
+/**
+ * Plugin allows to delete a customer account from Magento.
+ * Delete must be confirmed with token fetched from email message
+ * @param config
+ * @param db
+ * @param router
+ * @param cache
+ * @param apiStatus
+ * @param apiError
+ * @param getRestApiClient
+ * @returns {{router: *, route: string, pluginName: string, domainName: string}}
+ */
 module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
-    console.warn('----> Plugin initialized');
-    
+    const createMage2RestClient = () => {
+        const client = getRestApiClient();
+        client.addMethods('deleteCustomer', (restClient) => {
+            const module = {};
+            module.notify = (customerId, token) => {
+                return restClient.post(`/kmk-customer/delete-account/notify/${customerId}`, token);
+            };
+            module.confirm = (customerId, token) => {
+              return restClient.post('/kmk-customer/delete-account/confirm', { customer_id: customerId, token }, token);
+            };
+
+            return module;
+        });
+
+        return client;
+    };
+
+    router.post('/:customerId', async (req, res) => {
+        const {customerId} = req.params;
+        const {token} = req.query;
+        try {
+            if (!token) { throw new Error('Customer token is invalid'); }
+            if (!customerId) { throw new Error(`Customer id is required`); }
+
+            const client = createMage2RestClient();
+            client.deleteCustomer.notify(customerId, token)
+                .then(response => apiStatus(res, response))
+                .catch(err => apiError(res, err))
+        } catch (e) {
+            apiError(res, e);
+        }
+    });
+
+    router.post('/confirm/:customerId', async (req, res) => {
+        const {customerId} = req.params;
+        const {token} = req.query;
+        try {
+            if (!token) { throw new Error('Delete token is invalid'); }
+            if (!customerId) { throw new Error(`Customer id is required`); }
+
+            const client = createMage2RestClient();
+            client.deleteCustomer.confirm(customerId, token)
+                .then(response => apiStatus(res, response))
+                .catch(err => apiError(res, err))
+        } catch (e) {
+            apiError(res, e);
+        }
+    });
+
     return {
-        domainName: '{{domainName}}',
-        pluginName: '{{pluginName}}',
-        route: '{{restRoute}}',
+        domainName: '@grupakmk',
+        pluginName: 'customer-delete',
+        route: '/customer-delete',
         router
     };
 };
